c1432edd684c1e4533189268d58b2d4c896a1709
Pedro <phls73376@gmail.com>
2019-04-22 12:29:04 -0300

    number insertion

--- project/ex48/ex48/lexicon.py
+++ project/ex48/ex48/lexicon.py
@@ -1,24 +1,32 @@
-def scan(direction):
+def scan(palavra):
     l_return = []
-    if( "north" in direction):
+    palavras_separadas = palavra.split(' ')
+    if( "north" in palavras_separadas):
         l_return.append(("direction", "north"))
-    if( "south" in direction):
+    if( "south" in palavras_separadas):
         l_return.append(("direction","south"))
-    if( "east" in direction):
+    if( "east" in palavras_separadas):
         l_return.append(("direction","east"))
-    if( "west" in direction):
+    if( "west" in palavras_separadas):
         l_return.append(("direction","west"))
-    if( "go" in direction):
+    if( "go" in palavras_separadas):
         l_return.append(("verb","go"))
-    if( "kill" in direction):
+    if( "kill" in palavras_separadas):
         l_return.append(("verb","kill"))
-    if( "eat" in direction):
+    if( "eat" in palavras_separadas):
         l_return.append(("verb","eat"))
-    if( "the" in direction):
+    if( "the" in palavras_separadas):
         l_return.append(("stop","the"))
-    if( "in" in direction):
+    if( "in" in palavras_separadas):
         l_return.append(("stop","in"))
-    if( "of" in direction):
+    if( "of" in palavras_separadas):
         l_return.append(("stop","of"))
+    if( "bear" in palavras_separadas):
+        l_return.append(("noun","bear"))
+    if( "princess" in palavras_separadas):
+        l_return.append(("noun","princess"))
+    for x in palavras_separadas :
+        if x.isnumeric():
+            l_return.append(("number", int(x)))
+
     return l_return
-    
--- project/ex48/tests/ex48_tests.py
+++ project/ex48/tests/ex48_tests.py
@@ -22,17 +22,17 @@ def test_stops():
                             ('stop', 'in'),
                             ('stop', 'of')])
 
-# def test_nouns():
-#     assert_equal(lexicon.scan("bear"), [('noun', 'bear')])
-#     result = lexicon.scan("bear princess")
-#     assert_equal(result,   [('noun', 'bear'),
-#                             ('noun', 'princess')])
+def test_nouns():
+    assert_equal(lexicon.scan("bear"), [('noun', 'bear')])
+    result = lexicon.scan("bear princess")
+    assert_equal(result,   [('noun', 'bear'),
+                            ('noun', 'princess')])
 
-# def test_numbers():
-#     assert_equal(lexicon.scan("1234"), [('number', 1234)])
-#     result = lexicon.scan("3 91234")
-#     assert_equal(result,   [('number', 3),
-#                             ('number', 91234)])
+def test_numbers():
+    assert_equal(lexicon.scan("1234"), [('number', 1234)])
+    result = lexicon.scan("3 91234")
+    assert_equal(result,   [('number', 3),
+                            ('number', 91234)])
 
 # def test_errors():
 #     assert_equal(lexicon.scan("ASDFADFASDF"),
